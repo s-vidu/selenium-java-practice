b3a76f7c5a1d8350a3452e43023bc75afdcef8a6
Vidumali Dahanayake <vidumalid@gmail.com>
2025-09-30 22:05:46 +0530

    Updated form elements testing

--- src/test/java/formElements.java
+++ src/test/java/formElements.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
@@ -14,16 +15,31 @@ public class formElements {
         driver.findElement(By.id("name")).sendKeys("Vidumali");
         driver.findElement(By.id("email")).sendKeys("vidumalid@gmail.com");
         driver.findElement(By.id("phone")).sendKeys("0768801260");
+        driver.findElement(By.id("textarea")).sendKeys("Yakkalamulla, Galle");
+        driver.findElement(By.id("datepicker")).click();
+        driver.findElement(By.xpath("//a[text()='9']")).click();
+
+        driver.findElement(By.id("txtDate")).click();
+        driver.findElement(By.xpath("//a[text()='9']")).click();
+
+        driver.findElement(By.id("start-date")).sendKeys("09/03/2025");
+        driver.findElement(By.id("end-date")).sendKeys("09/05/2025");
 
 // Radio button
-        driver.findElement(By.id("male")).click();
+        driver.findElement(By.id("female")).click();
 
 // Checkbox
         driver.findElement(By.id("sunday")).click();
 
 // Dropdown
         Select country = new Select(driver.findElement(By.id("country")));
-        country.selectByVisibleText("Canada");
+        country.selectByVisibleText("India");
+
+        driver.findElement(By.cssSelector(".submit-btn")).click();
+
+// Example: check if result appears after clicking
+        WebElement result = driver.findElement(By.id("result"));
+        System.out.println("Result: " + result.getText());
 
 
     }
